80ae663958f13ac0041ae9d618db0e9b327193ca
Christina Mercando <christinasemail@gmail.com>
2013-04-19 16:36:28 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ringly::Application.config.secret_token = '2ecd3ae5d062ee275ab595bc0a1a80edf98ed6d5ba9427b27bc86d56be7396bf94082aa49d20c3573b24b636e6a090c3bef1a8e2886f211cce3eb41488a3a273'
